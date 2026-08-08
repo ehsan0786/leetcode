7bac1add963561e3b0851a9235eb3be1134694ff
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-08-08 12:52:38 +0530

    Time: 6 ms (37.65%) | Memory: 19.1 MB (19.97%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    vector<int> topologocalSort(unordered_map<int,vector<int>> &adj,int n,vector<int> &indegree){
-        queue<int> que;
-        vector<int> result;
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                que.push(i);
-                cnt++;
-                result.push_back(i);
+    bool hasCycle;
+    void DFS(unordered_map<int,vector<int>> &adj,int u,vector<bool> &visited,stack<int> &st,vector<bool> &inRecursion){
+        visited[u]=true;
+        inRecursion[u] = true;
+        for(auto &v:adj[u]){
+            if(inRecursion[v] == true){
+                hasCycle = true;
+                return;
+            }
+            if(!visited[v]){
+                DFS(adj,v,visited,st,inRecursion);
             }
         }
-        while(!que.empty()){
-            int u = que.front();
-            que.pop();
-            for(auto &v : adj[u]){
-                indegree[v]--;
-                if(indegree[v] == 0){
-                    que.push(v);
-                    cnt++;
-                    result.push_back(v);
-                }
-            }
-        }
-        if(cnt == n) return result;
-        //means we had a cycle and we couldn't complete all courses
-        return {};
+        st.push(u);
+        inRecursion[u]=false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>> adj;
-        vector<int> indegree(numCourses,0);
-        for(auto &vec :prerequisites){
-            int u = vec[0];
-            int v = vec[1];
-            //{a,b}
-            //b-->a
-            adj[v].push_back(u);
-            indegree[u]++;
+        hasCycle = false;
+        for(auto &vec : prerequisites){
+            int a = vec[0];
+            int b = vec[1];
+            adj[b].push_back(a);
         }
-        return topologocalSort(adj,numCourses,indegree);
+        vector<bool> visited(numCourses,false);
+        vector<bool> inRecursion(numCourses,false);
+        stack<int> st;
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                DFS(adj,i,visited,st,inRecursion);
+            }
+        }
+        if(hasCycle == true) return {};
+        vector<int> result;
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        return result;
     }
 };
