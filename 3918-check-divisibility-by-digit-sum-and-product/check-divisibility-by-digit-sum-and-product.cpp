edfa0f3d224e9f38691997bef5da5ab52c33f8c1
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-08-22 11:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.17%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int sum(int n){
-       int x = 0;
-        while(n > 0){
-            int last_dig = n%10;
-            x += last_dig;
-            n /= 10;
-        }
-        return x;
-    }
-
-    int product(int n){
-       int x = 1;
-       while(n > 0){
-         int last_dig = n%10;
-            x *= last_dig;
-            n /= 10;
-       }
-       return x;
-    }
-
     bool checkDivisibility(int n) {
-        int dig_sum = sum(n);
-        int dig_prod = product(n);
-        return (n % (dig_sum + dig_prod) == 0);
+        int digitSum = 0;
+        int digitProduct = 1;
+        int original = n;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+
+            digitSum += digit;
+            digitProduct *= digit;
+        }
+
+        return original % (digitSum + digitProduct) == 0;
     }
 };
