97838fa4d89a19a3fe4bb1ffaee3fc01be0dfc37
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-08-24 23:38:54 +0530

    Time: 96 ms (86.38%) | Memory: 95.3 MB (49.63%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int n;
-    int t[100001];
-    int solve(int i,vector<int> &prefixSum){
-        if(i==n-1) return prefixSum[n-1];
-        if(t[i] != -1) return t[i];
-        int take = prefixSum[i] - solve(i+1,prefixSum);
-        int skip = solve(i+1,prefixSum);
-        return t[i] = max(take,skip);
-    }
     int stoneGameVIII(vector<int>& stones) {
         n = stones.size();
-        memset(t,-1,sizeof(t));
         vector<int> prefixSum(n,0);
         prefixSum[0] = stones[0];
 
