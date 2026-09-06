2d0c28bf6bd0496581f85403d66a9d2c219909db
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-09-07 00:31:20 +0530

    Time: 926 ms (94.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+select 
+    ST.student_id,
+    ST.student_name,
+    SB.subject_name,
+    count(EX.subject_name) as attended_exams
+from Students as ST
+cross join Subjects as SB
+left join Examinations as EX
+on ST.student_id = EX.student_id and SB.subject_name = EX.subject_name
+group by ST.student_id , ST.student_name,SB.subject_name
+order by student_id,subject_name
+
+-- A CROSS JOIN combines every row of Table A with every row of Table B.
+
+-- A **CROSS JOIN** combines **every row of left Table  with every row of right Table** 
+-- (mxn)
