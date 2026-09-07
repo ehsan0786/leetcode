b9450dab843021b1c3892ff09d607c13921e9ff1
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-09-07 18:55:43 +0530

    Time: 750 ms (49.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+select 
+    s.user_id,        
+    IFNULL(round(sum(c.action = 'confirmed')/count(c.user_id),2),0.00) as confirmation_rate 
+from Signups as s
+left join Confirmations as c
+on s.user_id  = c.user_id 
+group by s.user_id   
+
+
+-- SUM(c.action = 'confirmed')
+-- means:
+-- "SUM Count how many times the condition is TRUE.
+
+-- A Foreign Key is a column that connects one table to another table.
