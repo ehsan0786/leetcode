eb2fc6ab691723aead2d42c3d474f0bcca4003d5
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-09-07 19:11:17 +0530

    Time: 899 ms (18.33%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -2,7 +2,7 @@
 
 select 
     s.user_id,        
-    IFNULL(round(sum(c.action = 'confirmed')/count(c.user_id),2),0.00) as confirmation_rate 
+    coalesce(round(sum(c.action = 'confirmed')/count(c.user_id),2),0.00) as confirmation_rate 
 from Signups as s
 left join Confirmations as c
 on s.user_id  = c.user_id 
