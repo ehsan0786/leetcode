498a2deae5fee4f346b85eec232cf22df760a613
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-09-12 19:39:04 +0530

    Time: 508 ms (97.44%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -4,12 +4,12 @@ select
     employee_id,
     department_id 
 from Employee 
-where employee_id in (
+where primary_flag = 'Y' or employee_id in (
                         select employee_id
                         from Employee
                         group by employee_id 
                         having count(*) = 1
-                     ) or primary_flag = 'Y'
+                     ) 
 
 
 
