fc88941d429e437fef76c089d16b349b52e34977
Ehsan Ansari <ehsanansari0022@gmail.com>
2026-09-18 16:25:19 +0530

    Time: 394 ms (92.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+select *
+from Patients 
+where conditions like "DIAB1%" or  conditions like "% DIAB1%"
+
+
+
+
+
+
+
+-- LIKE → pattern खोजने के लिए
+-- % → कोई भी 0 या ज्यादा characters
